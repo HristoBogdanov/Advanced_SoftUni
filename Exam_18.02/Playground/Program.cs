f0f5b59c70a840114a32ff004061222c20d9d012
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-10-12 08:56:35 +0300

    Add files via upload

---
+++ Exam_18.02/Playground/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split();
+        int n = int.Parse(input[0]);
+        int m = int.Parse(input[1]);
+        int moves = 0;
+        int opponentsTouched = 0;
+        char[,] playground = new char[n, m];
+        int playerRow = 0;
+        int playerCol = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] row = Console.ReadLine().Split();
+            for (int j = 0; j < m; j++)
+            {
+                char current = row[j][0];
+                playground[i, j] = current;
+                if (current == 'B')
+                {
+                    playerRow = i;
+                    playerCol = j;
+                }
+            }
+        }
+
+        while (true)
+        {
+            string command = Console.ReadLine();
+            if (command == "Finish")
+            {
+                Console.WriteLine("Game over!");
+                Console.WriteLine($"Touched opponents: {opponentsTouched} Moves made: {moves}");
+                break;
+            }
+
+            int newRow = playerRow;
+            int newCol = playerCol;
+            switch (command)
+            {
+                case "up":
+                    newRow--;
+                    break;
+                case "down":
+                    newRow++;
+                    break;
+                case "left":
+                    newCol--;
+                    break;
+                case "right":
+                    newCol++;
+                    break;
+            }
+
+            if (newRow < 0 || newRow >= n || newCol < 0 || newCol >= m)
+            {
+                continue;
+            }
+
+            char newPosition = playground[newRow, newCol];
+            if (newPosition == 'O')
+            {
+                continue;
+            }
+
+            moves++;
+
+            if (newPosition == 'P')
+            {
+                opponentsTouched++;
+                playground[newRow, newCol] = '-';
+            }
+
+            playerRow = newRow;
+            playerCol = newCol;
+
+            if (opponentsTouched == 3)
+            {
+                Console.WriteLine("Game over!");
+                Console.WriteLine($"Touched opponents: {opponentsTouched} Moves made: {moves}");
+                break;
+            }
+        }
+    }
+}
---
+++ Exam_18.02/Stacks_Queues/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
+
+
+Queue<int> textile = new(
+    Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse));
+
+Stack<int> medicaments = new(
+    Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse));
+
+
+Dictionary<string, int> craftedItems = new();
+craftedItems.Add("Patch", 0);
+craftedItems.Add("Bandage", 0);
+craftedItems.Add("MedKit", 0);
+
+
+while (textile.Any() && medicaments.Any())
+{
+    int currentTextile = textile.Dequeue();
+    int currentMedicament = medicaments.Pop();
+
+    int sum = currentMedicament + currentTextile;
+
+    if(sum==30)
+    {
+        craftedItems["Patch"]++;
+    }
+    else if(sum==40)
+    {
+        craftedItems["Bandage"]++;
+    }
+    else if(sum==100)
+    {
+        craftedItems["MedKit"]++;
+    }
+    else if(sum>100)
+    {
+        craftedItems["MedKit"]++;
+
+        if(medicaments.Any())
+        {
+            int nextMedicament = medicaments.Pop();
+            medicaments.Push(nextMedicament + (sum - 100));
+        }
+
+    } 
+    else
+    {
+        medicaments.Push(currentMedicament + 10);
+    }
+}
+if(!textile.Any() && !medicaments.Any())
+{
+    Console.WriteLine("Textiles and medicaments are both empty.");
+}
+else if(!textile.Any())
+{
+    Console.WriteLine("Textiles are empty.");
+}
+else
+{
+    Console.WriteLine("Medicaments are empty.");
+}
+
+var sorted = craftedItems.OrderByDescending(x => x.Value).ThenBy(n=>n.Key).ToDictionary(x => x.Key, x => x.Value);
+
+foreach (var kvp in sorted.Where(x=>x.Value>0))
+{
+    Console.WriteLine($"{kvp.Key} - {kvp.Value}");
+}
+
+if(medicaments.Any())
+{
+    Console.WriteLine($"Medicaments left: {String.Join(", ",medicaments)}");
+}
+if (textile.Any())
+{
+    Console.WriteLine($"Textiles left: {String.Join(", ", textile)}");
+}
