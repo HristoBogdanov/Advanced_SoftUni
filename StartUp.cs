3a5e73be22f4fcce8689b5ca14b674ebe9751a0a
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2023-11-29 08:59:26 +0200

    Add files via upload

--- StartUp.cs
+++ StartUp.cs
@@ -1,4 +1,4 @@
-﻿namespace UniversityLibrary
+﻿namespace RobotFactory
 {
     public class StartUp
     {
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,66 +1,110 @@
-namespace UniversityLibrary.Test
+using NUnit.Framework;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace RobotFactory.Tests
 {
-    using NUnit.Framework;
     public class Tests
     {
-        private UniversityLibrary testLibrary;
-        private TextBook testBook;
+        private Factory factory;
         [SetUp]
         public void Setup()
         {
-            testLibrary = new();
-            testBook = new("TestTitle", "TestAuthor", "Drama");
+            factory = new("TestFactory", 2);
         }
 
         [Test]
-        public void ListInitializesCorrectly()
+        public void ConstructorInitializingProperly()
         {
-            Assert.IsNotNull(testLibrary);
-        }
-        [Test]
-        public void AddTextBookMethodWorksCorrrectly()
-        {
-            testLibrary.AddTextBookToLibrary(testBook);
-
-            Assert.AreEqual(testLibrary.Catalogue.Count, 1);
-        }
-        [Test]
-        public void BookInventoryNumberWorksCorrectly()
-        {
-            Assert.AreEqual(testBook.InventoryNumber, 0);
-        }
-        [Test]
-        public void LoandedBookNotReturnedReturnMessage()
-        {
-            testBook.Holder = "John";
-            testLibrary.AddTextBookToLibrary(testBook);
-
-            Assert.AreEqual(testLibrary.LoanTextBook(1, "John"), "John still hasn't returned TestTitle!");
-        }
-        [Test]
-        public void LoandedBookSuccessfully()
-        {
-            testLibrary.AddTextBookToLibrary(testBook);
-
-            Assert.AreEqual(testLibrary.LoanTextBook(1, "John"), "TestTitle loaned to John.");
-        }
-        [Test]
-        public void ReturningABookGetsRidOfHolder()
-        {
-            testLibrary.AddTextBookToLibrary(testBook);
-            testLibrary.LoanTextBook(1, "John");
-            testLibrary.ReturnTextBook(1);
-
-            Assert.AreEqual(testBook.Holder, string.Empty);
+            Assert.IsNotNull(factory);
+            Assert.AreEqual(factory.Name, "TestFactory");
+            Assert.AreEqual(factory.Capacity, 2);
+            Assert.IsNotNull(factory.Robots);
+            Assert.IsNotNull(factory.Supplements);
         }
 
         [Test]
-        public void ReturningBookSuccessfullMessage()
+        public void SuccessfullyProduceRobotWhenCapacityIsNotReached()
         {
-            testLibrary.AddTextBookToLibrary(testBook);
-            testLibrary.LoanTextBook(1, "John");
-
-            Assert.AreEqual(testLibrary.ReturnTextBook(1), "TestTitle is returned to the library.");
+            Assert.AreEqual(factory.ProduceRobot("Robot1", 10, 1), "Produced --> Robot model: Robot1 IS: 1, Price: 10.00");
         }
+        [Test]
+        public void ProducingARobbotAddsItToTheCollection()
+        {
+            factory.ProduceRobot("Robot1", 10, 1);
+            Assert.AreEqual(1, factory.Robots.Count);
+        }
+        [Test]
+        public void NotAddingRobotToCollectionIfCapacityIsReached()
+        {
+            factory.ProduceRobot("Robot1", 10, 1);
+            factory.ProduceRobot("Robot2", 12, 1);
+            factory.ProduceRobot("Robot3", 13, 1);
+            factory.ProduceRobot("Robot4", 14, 1);
+
+            Assert.AreEqual(2, factory.Robots.Count);
+        }
+
+        [Test]
+        public void CorrectMessageShowsIfCapacityIsReached()
+        {
+            factory.ProduceRobot("Robot1", 10, 1);
+            factory.ProduceRobot("Robot2", 12, 1);
+
+            Assert.AreEqual(factory.ProduceRobot("Robot3", 10, 1), "The factory is unable to produce more robots for this production day!");
+        }
+        [Test]
+        public void AddingSupplementToCollectionWhenCapacityIsNotReached()
+        {
+            factory.ProduceSupplement("Supplement1", 1);
+
+            Assert.AreEqual(1, factory.Supplements.Count);
+        }
+        [Test]
+        public void CorrectMessageShowsWhenSupplementIsAdded()
+        { 
+            Assert.AreEqual(factory.ProduceSupplement("Supplement1", 1), "Supplement: Supplement1 IS: 1");
+        }
+
+        [Test]
+        public void SellRobotShowsTheCorrectRobot()
+        {
+
+            factory.ProduceRobot("Robot1",20, 1);
+            factory.ProduceRobot("Robot2", 3, 1);
+            Robot robot = factory.Robots[1];
+
+            Assert.AreEqual(factory.SellRobot(10), robot);
+        }
+        [Test]
+        public void UpgradingARobotReturnsFalseIfContainsSupplement()
+        {
+            Robot robot = new("Robot1", 20, 1);
+            Supplement supplement = new("Supplement1", 1);
+
+            robot.Supplements.Add(supplement);
+
+            Assert.AreEqual(factory.UpgradeRobot(robot, supplement), false);
+        }
+        [Test]
+        public void UpgradingARobotReturnsFalseIfInterfaceStandartDoesNotMatch()
+        {
+            Robot robot = new("Robot1", 20, 1);
+            Supplement supplement = new("Supplement1", 2);
+
+
+            Assert.AreEqual(factory.UpgradeRobot(robot, supplement), false);
+        }
+        [Test]
+        public void UpgradingARobotReturnsTrueAndUpdatesCollection()
+        {
+            Robot robot = new("Robot1", 20, 1);
+            Supplement supplement = new("Supplement1", 1);
+
+
+            Assert.AreEqual(factory.UpgradeRobot(robot, supplement), true);
+            Assert.AreEqual(robot.Supplements.Count, 1);
+        }
+
     }
 }
